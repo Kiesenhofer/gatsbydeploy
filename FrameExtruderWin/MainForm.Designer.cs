5cdd846830e660240d56b2c0fd10ff85559cf004
Kiesenhofer <96688728+Kiesenhofer@users.noreply.github.com>
2020-02-12 10:47:54 +0000

    Wiki

--- FrameExtruderWin/MainForm.Designer.cs
+++ FrameExtruderWin/MainForm.Designer.cs
@@ -3,4 +3,10 @@
     partial class MainForm
     {
         /// <summary>
-        
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+  
