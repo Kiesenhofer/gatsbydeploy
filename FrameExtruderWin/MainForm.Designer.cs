681725e1b655245009136810adcaba9529180aae
Kiesenhofer <96688728+Kiesenhofer@users.noreply.github.com>
2020-02-15 14:42:08 +0000

    prev commit.

--- FrameExtruderWin/MainForm.Designer.cs
+++ FrameExtruderWin/MainForm.Designer.cs
@@ -9,4 +9,8 @@
 
         /// <summary>
         /// Clean up any resources being used.
-  
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (dis
