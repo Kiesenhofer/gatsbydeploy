c89e820171217a283d1103415c052b39f51a3a1c
Kiesenhofer <96688728+Kiesenhofer@users.noreply.github.com>
2020-02-15 19:27:25 +0000

    bump version to 1.0.2

--- FrameExtruderWin/MainForm.Designer.cs
+++ FrameExtruderWin/MainForm.Designer.cs
@@ -20,4 +20,10 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated 
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        privat
