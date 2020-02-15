ac81a8abab316d07e63c7ab99c25a3b9aac97ce3
Kiesenhofer <96688728+Kiesenhofer@users.noreply.github.com>
2020-02-15 18:17:13 +0000

    Initial commit of script

--- FrameExtruderWin/MainForm.Designer.cs
+++ FrameExtruderWin/MainForm.Designer.cs
@@ -13,4 +13,11 @@
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (dis
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated 
