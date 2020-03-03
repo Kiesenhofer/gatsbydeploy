385988ed6b82232ea29a1837d11b85d6ffb0f500
Kiesenhofer <96688728+Kiesenhofer@users.noreply.github.com>
2020-03-03 14:53:26 +0000

    more docs

--- FrameExtruderWin/MainForm.Designer.cs
+++ FrameExtruderWin/MainForm.Designer.cs
@@ -66,4 +66,11 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(6, 56);
             this.label1.Name = "label1";
-            this.label1.Size = new Sys
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Frame Height:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+         
