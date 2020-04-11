c47a568bba06c079feb863a8d4a0f01ccaf9a86b
Kiesenhofer <96688728+Kiesenhofer@users.noreply.github.com>
2020-04-11 12:50:34 +0000

    Work on strand parsing doc

--- FrameExtruderWin/MainForm.Designer.cs
+++ FrameExtruderWin/MainForm.Designer.cs
@@ -219,4 +219,8 @@
             this.BtnSelectManually.Click += new System.EventHandler(this.BtnSelectManually_Click);
             // 
             // label3
-     
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(134, 464);
+            this.
