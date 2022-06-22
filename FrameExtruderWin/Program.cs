ab5e6b2388ebe50fa2a83e47184f7850f22eb43f
Kiesenhofer <96688728+Kiesenhofer@users.noreply.github.com>
2022-06-22 15:54:44 +0000

    Initial package

--- FrameExtruderWin/Program.cs
+++ FrameExtruderWin/Program.cs
@@ -1,2 +1,12 @@
 ﻿using System;
-using System.ComponentModel
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FrameExtruderWin {
+    static class Program {
+
+        [STAThread]
+        static void Main(string[] args) {
+            Application.Enabl
