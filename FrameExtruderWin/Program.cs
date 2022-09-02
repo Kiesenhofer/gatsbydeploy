0da83f4746e4faff61e58243fe6bb92a5277b0e3
Kiesenhofer <96688728+Kiesenhofer@users.noreply.github.com>
2022-09-02 14:45:35 +0000

    Update visual tests

--- FrameExtruderWin/Program.cs
+++ FrameExtruderWin/Program.cs
@@ -56,4 +56,5 @@ namespace FrameExtruderWin {
 
                         // corners TL, TR, BL, BR...
                         extrudedSheet.DrawImage(shin, new Rectangle(destX, destY, 1, 1), new Rectangle(srcX, srcY, 1, 1), GraphicsUnit.Pixel);
-                        e
+                        extrudedSheet.DrawImage(shin, new Rectangle(destX + width + 1, destY, 1, 1), new Rectangle(srcX + width - 1, srcY, 1, 1), GraphicsUnit.Pixel);
+                        extrudedSheet.DrawImage(shin, new Rec
