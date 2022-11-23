9b834cc46c30bd5345acb24c6408d0da9fd4876d
Kiesenhofer <96688728+Kiesenhofer@users.noreply.github.com>
2022-11-23 09:57:28 +0000

    Создан проект для IDEA

--- FrameExtruderWin/Properties/Resources.Designer.cs
+++ FrameExtruderWin/Properties/Resources.Designer.cs
@@ -63,4 +63,11 @@ namespace FrameExtruderWin.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitma
+        internal static System.Drawing.Bitmap back {
+            get {
+                object obj = ResourceManager.GetObject("back", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /
