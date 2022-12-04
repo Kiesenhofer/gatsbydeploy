cb58604fdbb78537cc1cfdb2aabe1b2c239f171c
Kiesenhofer <96688728+Kiesenhofer@users.noreply.github.com>
2022-12-04 19:26:29 +0000

    Update InteractiveThreeRenderer.js

--- FrameExtruderWin/Properties/Settings.Designer.cs
+++ FrameExtruderWin/Properties/Settings.Designer.cs
@@ -13,4 +13,5 @@ namespace FrameExtruderWin.Properties
 
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Com
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.
