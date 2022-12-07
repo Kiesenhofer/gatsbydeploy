bd3fa868cf6e2f4d7da09650b48926ae4f393fde
Kiesenhofer <96688728+Kiesenhofer@users.noreply.github.com>
2022-12-07 08:40:27 +0000

    del

--- FrameExtruderWin/Properties/Settings.Designer.cs
+++ FrameExtruderWin/Properties/Settings.Designer.cs
@@ -14,4 +14,9 @@ namespace FrameExtruderWin.Properties
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+
+        public static Settings Default
